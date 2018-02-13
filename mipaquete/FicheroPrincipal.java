cae35fdebe030f25ad9e8d9608bdccc5b0388f25
kekote169 <36438413+kekote169@users.noreply.github.com>
2018-02-13 10:22:00 +0100

    Add files via upload

---
+++ mipaquete/FicheroPrincipal.java
@@ -0,0 +1,10 @@
+package com.mipaquete;
+
+public class FicheroPrincipal {
+
+	public static void main(String[] carol) {
+		System.out.println("Programa de Ejemplos: ");
+		EjemplosBooleanos.funcionPrincipal();
+	}
+
+}
